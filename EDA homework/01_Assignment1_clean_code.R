1b680148cdb23711fcb01ee12ed3141ca44c3591
AzarMahmudov <azer.mahmudov.7@gmail.com>
2022-04-01 11:03:10 +0200

    HW1

---
+++ EDA homework/01_Assignment1_clean_code.R
@@ -0,0 +1,9 @@
+tempratures = c(3,6,10,14)
+weights = c(1,0.8,1.2,1)
+
+library(data.table)
+
+multiply = function(first_number, second_number){
+  first_number * second_number
+}
+results <- multiply(tempratures, weights)
